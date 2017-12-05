8b5af679291aa2b5d325507266df65049b7a9d31
xwp <xwp_fullstack@163.com>
2017-12-05 16:27:00 +0800

    配置了debug,配置了SQLAlchemy

--- manage.py
+++ manage.py
@@ -2,9 +2,16 @@
 from flask import Flask
 app=Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+
 app.route('/')
 def index():
     return 'index'
 
+class Config():
+    DEBUG=True
+    db = SQLAlchemy(app)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()

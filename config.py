94f333a638ed97d08ffb4d72108eafcfadaac7c5
xwp <xwp_fullstack@163.com>
2017-12-05 23:20:57 +0800

    抽取开发环境和生产环境的配置

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 # -*- coding:utf-8 -*-
 import redis
-class Config():
+class Config(object):
     """配置信息类"""
-    # SECRET_KEY = "bxOaxOKPaeZaipSvq7rjfeYtYvG5jPvwIgGZsteuTGhTSrKAHh/Q6eGHQm2Yw671"
-    DEBUG=True
-
+    SECRET_KEY = "bxOaxOKPaeZaipSvq7rjfeYtYvG5jPvwIgGZsteuTGhTSrKAHh/Q6eGHQm2Yw671"
+    DEBUG = True
     # 数据库配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -18,3 +17,11 @@ class Config():
     SESSION_REDIS=redis.StrictRedis(host=REDIS_HOST,port=REDIS_PORT)
     SESSION_USE_SINGER =True
     PERMANENT_SESSION_LIFETIME = 86400
+
+class DevelopmentConfig(Config):
+    """开发环境下的配置"""
+    DEBUG = True
+
+class ProductionConfig(Config):
+    """生产环境下的配置"""
+    pass

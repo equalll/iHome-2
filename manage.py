39c8e5f7ab76a02a406b00c15fe61ddf8160f4e3
xwp <xwp_fullstack@163.com>
2017-12-05 20:20:18 +0800

    加入了token

--- manage.py
+++ manage.py
@@ -2,12 +2,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import redis
+from flask_wtf.csrf import CSRFProtect
 
 class Config():
     """配置信息类"""
+    SECRET_KEY = "bxOaxOKPaeZaipSvq7rjfeYtYvG5jPvwIgGZsteuTGhTSrKAHh/Q6eGHQm2Yw671"
     DEBUG=True
+
     # 数据库配置
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/ihome'
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/ihome"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # redis 连接配置
     REDIS_HOST="127.0.0.1"
@@ -15,12 +18,17 @@ class Config():
 
 app=Flask(__name__)
 app.config.from_object(Config)
+#初始化数据库链接
 db = SQLAlchemy(app)
+# 初始化redis
 redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-app.route('/index')
+#kaiqi开启CSRF 保护
+csrf = CSRFProtect(app)
+
+@app.route("/",methods=["GET","POST"])
 def index():
     redis_store.set('name','xiaoruirui')
-    return 'index'
+    return "index"
 
 
 if __name__ == '__main__':

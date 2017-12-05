6e80be3ee561c1bb0fe5adc8e258d73467e67162
xwp <xwp_fullstack@163.com>
2017-12-05 19:32:40 +0800

    配置了redis和数据库

--- manage.py
+++ manage.py
@@ -1,16 +1,26 @@
 # -*- coding:utf-8 -*-
 from flask import Flask
-app=Flask(__name__)
-
 from flask_sqlalchemy import SQLAlchemy
-
-app.route('/')
-def index():
-    return 'index'
+import redis
 
 class Config():
+    """配置信息类"""
     DEBUG=True
-    db = SQLAlchemy(app)
+    # 数据库配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/ihome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # redis 连接配置
+    REDIS_HOST="127.0.0.1"
+    REDIS_PORT = 6379
+
+app=Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
+app.route('/index')
+def index():
+    redis_store.set('name','xiaoruirui')
+    return 'index'
 
 
 if __name__ == '__main__':

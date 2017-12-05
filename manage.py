268e67d6653fd6149462657abd9b7e1a441ccf43
yujinneng <tjut316yjn@163.com>
2017-12-06 00:45:42 +0800

    修改了作者3

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ manager.add_command('db',MigrateCommand)
 def index():
     session["name"] ="xiaohua"
     # redis_store.set('name','xiaoruirui')
-    return "index yujjinneng22"
+    return "index yujjinneng222"
 
 
 if __name__ == '__main__':

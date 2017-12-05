faa69d95d8abea218114945f26f4385679b7bce0
xwp <xwp_fullstack@163.com>
2017-12-05 22:37:53 +0800

    集成了flask_migrate，迁移

--- manage.py
+++ manage.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import redis
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
+from flask_script import Manager
+from flask_migrate import Migrate,MigrateCommand
 
 class Config():
     """配置信息类"""
@@ -34,6 +36,11 @@ redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 csrf = CSRFProtect(app)
 # 给当前app的session设置保存路径
 Session(app)
+
+manager = Manager(app)
+#集成数据库的迁移
+Migrate(app,db)
+manager.add_command('db',MigrateCommand)
 
 @app.route("/",methods=["GET","POST"])
 def index():

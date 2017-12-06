61e65863f082f7e58395e783becb84f2910e72c4
yujinneng <tjut316yjn@163.com>
2017-12-06 23:58:14 +0800

    日志功能的集成

--- iHome/__init__.py
+++ iHome/__init__.py
@@ -1,27 +1,35 @@
 # -*- coding:utf-8 -*-
 import redis
+import logging
+from logging.handlers import RotatingFileHandler
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
 from config import config
-# app=Flask(__name__)
-# app.config.from_object(Config)
-
-# #初始化数据库链接
-# db = SQLAlchemy(app)
-# # 初始化redis
-# redis_store = redis.StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
-# #kaiqi开启CSRF 保护
-# csrf = CSRFProtect(app)
-# # 给当前app的session设置保存路径
-# Session(app)
-
 # 初始化redis
 redis_store =None
 # 初始化数据库连接
 db = SQLAlchemy()
 csrf=CSRFProtect()
+
+# 初始化redis
+redis_store = None
+# 初始化数据库连接
+db = SQLAlchemy()
+# 开启CSRF保护
+csrf = CSRFProtect()
+
+# 设置日志的记录等级
+logging.basicConfig(level=logging.DEBUG)  # 调试debug级
+# 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024*1024*100, backupCount=10)
+# 创建日志记录的格式                 日志等级    输入日志信息的文件名 行数    日志信息
+formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+# 为刚创建的日志记录器设置日志记录格式
+file_log_handler.setFormatter(formatter)
+# 为全局的日志工具对象（flask app使用的）添加日志记录器
+logging.getLogger().addHandler(file_log_handler)
 
 def create_app(config_name):
     """工厂方法：工厂模式，通过工厂方法去根据传入的配置创建与该配置相关的app"""
@@ -38,7 +46,7 @@ def create_app(config_name):
     # # 给当前app的session设置保存路径
     Session(app)
     # 注册api接口的蓝图
-    from api_1_0 import api
+    from iHome.api_1_0 import api
     app.register_blueprint(api)
     return app
 
--- iHome/api_1_0/index.py
+++ iHome/api_1_0/index.py
@@ -1,10 +1,11 @@
 from . import api
 from flask import session
+import logging
 from iHome import redis_store
 
 @api.route("/index",methods=["GET","POST"])
 def index():
     session["name"] ="xiaohua"
-
+    logging.debug('DEBUG')
     redis_store.set('name','xiaoruirui')
     return "index2"

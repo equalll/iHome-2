6ec662d19cbd1eccd1091ac0725ab05fa9543849
yujinneng <tjut316yjn@163.com>
2017-12-09 23:49:53 +0800

    登录验证装饰器的使用@functools.wraps

--- iHome/api_1_0/profile.py
+++ iHome/api_1_0/profile.py
@@ -6,24 +6,28 @@ from . import api
 from flask import request,session,current_app, jsonify
 from  iHome.models import User
 from iHome.constants import QINIU_DOMIN_PREFIX
-@api.route("/avatar",methods=["POST"])
+from flask import g
+from iHome.utils.common import login_required
 
+@api.route("/user/avatar",methods=["POST"])
+@login_required
 def save_image():
-    """
-0. TODO 判断用户是否登录
-1. 获取到上传的文件
-2. 再将文件上传到七牛云
-3. 将头像信息更新到当前用户的模型中
-4. 返回上传的结果<avatar_url>
-:return:
-"""
-    user_id =session.get("user_id")
+    # user_id =session.get("user_id")
+    user_id = g.user_id
     try:
+
         avatar_file = request.files.get("avatar").read()
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
-
+    #     """
+    # 0.  判断用户是否登录
+    # 1. 获取到上传的文件
+    # 2. 再将文件上传到七牛云
+    # 3. 将头像信息更新到当前用户的模型中
+    # 4. 返回上传的结果<avatar_url>
+    # :return:
+    # """
 
     try:
         url = storage_image(avatar_file)
@@ -31,21 +35,58 @@ def save_image():
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg="获取用户数据错误")
 
+
     try:
-        user = User.query.get("user_id")
+        user = User.query.get(user_id)
     except Exception as e:
         current_app.logger.error(e)
         return jsonify(errno=RET.USERERR, errmsg="用户不存在")
     # 设置用户模型相关数据
+    if not user:
+        return jsonify(errno=RET.USERERR, errmsg="用户不存在")
 
     user.avatar_url = url
     try:
-        db
+        db.session.commit()
     except Exception as e:
+        db.session.rollback()
         current_app.logger.error(e)
         return jsonify(errno=RET.DBERR, errmsg="保存用户数据错误")
 
     # 4. 返回上传的结果<avatar_url>
     return jsonify(errno=RET.OK, errmsg="OK",data={"avatar_url":QINIU_DOMIN_PREFIX+url})
 
+@api.route('/user/name',methods=["POST"])
+@login_required
+def save_user_name():
+    """
+     0. TODO 判断用户是否登录
+     1. 获取到传入参数
+     2. 将用户名信息更新到当前用户的模型中
+     3. 返回结果
+     :return:
+     """
+    data_dict = request.json
+    name = data_dict.get("name")
+    if not name:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不完整")
+
+    # user_id =session.get("user_id")
+    user_id = g.user_id
+    # try:
+    #     user_name=request.json.get("name")
+    # except Exception as e:
+    #     current_app.logger.error(e)
+    #     return jsonify()
+
+    user=User.query.get(user_id)
+    user.name=name
+    try:
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=RET.USERERR, errmsg="用户不存在")
+    session["name"]=name
+    return  jsonify(errno=RET.OK, errmsg="OK")
 
--- iHome/utils/common.py
+++ iHome/utils/common.py
@@ -1,6 +1,12 @@
 # -*- coding:utf-8 -*-
+import functools
 
+from flask import g
+from flask import session, jsonify
 from werkzeug.routing import BaseConverter
+
+from iHome.utils.response_code import RET
+
 
 class RegexConverter(BaseConverter):
     """自定义的正则转换器"""
@@ -8,3 +14,21 @@ class RegexConverter(BaseConverter):
     def __init__(self,url_map,*args):
         super(RegexConverter, self).__init__(url_map)
         self.regex = args[0]
+
+
+    # 被装饰器装饰的函数，默认会更改其__name__属性
+
+def login_required(f):
+    # 被装饰器装饰的函数，默认会更改其__name__属性
+    @functools.wraps(f)
+    # 防止装饰器去装饰函数的时候，被装饰的函数__name__属性被更改的问题
+    def wrap(*args,**kwargs):
+        user_id = session.get("user_id")
+        if not user_id:
+            return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+        else:
+            g.user_id=user_id
+            # 执行所装饰的函数并返回其响应
+            return f(*args,**kwargs)
+    return wrap
+

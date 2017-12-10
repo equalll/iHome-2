f2b2ae71a9dc98e5cd5964e25da5c8f5a5baba5c
yujinneng <tjut316yjn@163.com>
2017-12-10 21:22:55 +0800

    新发布房屋的后端逻辑

--- iHome/api_1_0/house.py
+++ iHome/api_1_0/house.py
@@ -1,11 +1,96 @@
 # -*- coding:utf8 -*-
 from flask import current_app, jsonify
+from flask import g
+from flask import request
 
-from iHome import redis_store
+from iHome import redis_store, db
 from iHome.constants import AREA_INFO_REDIS_EXPIRES
+from iHome.utils.common import login_required
 from iHome.utils.response_code import RET
 from . import api
-from iHome.models import Area
+from iHome.models import Area, House
+
+
+@api.route("/house",methods=["POST"])
+@login_required
+def save_new_house():
+    """
+    1. 接收参数并且判空
+    2. 将参数的数据保存到新创建house模型
+    3. 保存house模型到数据库
+    前端发送过来的json数据
+    {
+        "title":"",
+        "price":"",
+        "area_id":"1",
+        "address":"",
+        "room_count":"",
+        "acreage":"",
+        "unit":"",
+        "capacity":"",
+        "beds":"",
+        "deposit":"",
+        "min_days":"",
+        "max_days":"",
+        "facility":["7","8"]
+    }
+    :return:
+    """
+
+    # 1. 取到参数
+    user_id = g.user_id
+
+    json_dict = request.json
+    title = json_dict.get('title')
+    price = json_dict.get('price')
+    address = json_dict.get('address')
+    area_id = json_dict.get('area_id')
+    room_count = json_dict.get('room_count')
+    acreage = json_dict.get('acreage')
+    unit = json_dict.get('unit')
+    capacity = json_dict.get('capacity')
+    beds = json_dict.get('beds')
+    deposit = json_dict.get('deposit')
+    min_days = json_dict.get('min_days')
+    max_days = json_dict.get('max_days')
+
+    if not all(
+            [title, price, address, area_id, room_count, acreage, unit, capacity, beds, deposit, min_days, max_days]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
+    # 1.2 校验参数格式是否正确
+    try:
+        price = int(float(price)*100)
+        deposit = int(float(deposit)*100)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数有误")
+
+    house = House()
+    house.user_id = user_id
+    house.area_id = area_id
+    house.title = title
+    house.price = price
+    house.address = address
+    house.room_count = room_count
+    house.acreage = acreage
+    house.unit = unit
+    house.capacity = capacity
+    house.beds = beds
+    house.deposit = deposit
+    house.min_days = min_days
+    house.max_days = max_days
+
+    try:
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据保存错误")
+
+    return jsonify(errno=RET.OK, errmsg="ok", data={"house_id": house.id})
+)
+
+
 @api.route("/areas")
 def get_areas():
     """

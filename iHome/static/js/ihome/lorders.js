1273228c7e5cd919fc39e5ddba80c410306bac2d
yujinneng <tjut316yjn@163.com>
2017-12-13 20:10:07 +0800

    接受订单的前端逻辑的实现

--- iHome/static/js/ihome/lorders.js
+++ iHome/static/js/ihome/lorders.js
@@ -27,6 +27,31 @@ $(document).ready(function(){
             var orderId = $(this).parents("li").attr("order-id");
             $(".modal-accept").attr("order-id", orderId);
         });
+            $(".modal-accept").on("click",function () {
+                var orderId = $(".modal-accept").attr("order-id")
+                $.ajax({
+                    url:"/api/v1.0/orders",
+                    type:"put",
+                    contentType: "application/json",
+                    headers:{
+                        "X-CSRFToken":getCookie("csrf_token")
+                    },
+                    data:JSON.stringify({"order_id":orderId}),
+                    success:function (resp) {
+                        if(resp.errno=="0"){
+                            // 1. 设置订单状态的html
+                            $(".orders-list>li[order-id="+ orderId +"]>div.order-content>div.order-text>ul li:eq(4)>span").html("已接单");
+                            // 2. 隐藏接单和拒单操作
+                            $("ul.orders-list>li[order-id="+ orderId +"]>div.order-title>div.order-operate").hide();
+                            // 3. 隐藏弹出的框
+                            $("#accept-modal").modal("hide");
+                        }else{
+                            alert(resp.errmsg)
+                        }
+                    }
+                })
+
+            })
             $(".order-reject").on("click", function(){
             var orderId = $(this).parents("li").attr("order-id");
             $(".modal-reject").attr("order-id", orderId);
